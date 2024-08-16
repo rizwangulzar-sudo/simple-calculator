85d32b8ad53d3272e9542b48cc350751a6a19a7c
rizwangulzar-sudo <gulzwan@gmail.com>
2024-08-16 17:51:05 +0500

    i am in start of making calculator

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+// conditional statement.
+if (asnwer.operator === "Addition") {
+    console.log("your value is" + asnwer.firstNumber + asnwer.secondNumber);
+}
